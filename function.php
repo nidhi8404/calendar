144a4ddfdc80eeb31aa5352ac292aa4f908b8ee7
nidhi8404 <nidhi2shine@gmail.com>
2024-06-02 17:44:48 +0530

    add events

--- function.php
+++ function.php
@@ -10,5 +10,18 @@ if(!empty($_GET['type']) && $_GET['type'] == 'list'){
     }
     echo json_encode($arr);
 } 
+elseif(!empty($_GET['type']) && $_GET['type'] == 'add'){
+    $sql = "INSERT INTO `calendar_event_master`(`event_name`, `event_start_date`, `event_end_date`) 
+    VALUES ('" . $_POST['event_name'] . "' , '" . $_POST['event_start_date'] . "' ,'" . $_POST['event_end_date'] . "')";
+
+    if ($conn->query($sql) === TRUE) {
+        $json['status'] = true;
+        $json['message'] = "Event added successfully";
+    } else {
+        $json['status'] = false;
+        $json['message'] = "Error";
+    }
+    echo json_encode($json);
+}
 
 ?>
--- js/script.js
+++ js/script.js
@@ -24,16 +24,19 @@ function getEvent() {
           center: "title",
           right: "dayGridMonth,dayGridWeek,dayGridDay",
         },
-        initialDate:  new Date().toISOString().slice(0, 10),
+        initialDate: new Date().toISOString().slice(0, 10),
         navLinks: true, // can click day/week names to navigate views
         editable: true,
         dayMaxEvents: true, // allow "more" link when too many events
         selectable: true,
         events: events,
-        select: function(datetime){
-            $("#event_start_date").val(datetime.start.toISOString().split('T')[0]);
-            $("#event_end_date").val(datetime.end.toISOString().split('T')[0]);
-            $('#event_entry_modal').modal('show');
+        select: function (datetime) {
+            $(".clear-form").val('');
+          $("#event_start_date").val(
+            datetime.start.toISOString().split("T")[0]
+          );
+          $("#event_end_date").val(datetime.end.toISOString().split("T")[0]);
+          $("#event_entry_modal").modal("show");
         },
       });
 
@@ -41,4 +44,20 @@ function getEvent() {
     },
   });
 }
+
 getEvent();
+
+$("body").delegate("#submit_event_form", "submit", function (e) {
+  e.preventDefault();
+  $.ajax({
+    type: "POST",
+    url: "function.php?type=add",
+    data: $(this).serialize(),
+    dataType: "json",
+    success: function (data) {
+      alert(data.message);
+      $("#event_entry_modal").modal("hide");
+      getEvent();
+    },
+  });
+});
